419046a239f465851160de2a3dd8dab42e45cfc2
sachin kumar <sk2105@yahoo.com>
2024-11-27 14:34:41 +0530

    add analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import CouponView from './components/CouponView'
 import Header from './components/Header'
-
+import { Analytics } from '@vercel/analytics/react'
 
 function App() {
 
@@ -10,6 +10,7 @@ function App() {
 
       <Header />
       <CouponView/>
+      <Analytics />
       
     </div>
   )
